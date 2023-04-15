1ad5735813632440eccd5af81362043f40a7054e
Basit Manzoor <basit1107@gmail.com>
2023-04-15 20:44:08 +0530

    done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class RWOnly {
-    
+        private String name;
+
+        public String readOnly () {
+            return name;
+        }
+
+        public void writeOnly (String name) {
+            this.name = name;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
